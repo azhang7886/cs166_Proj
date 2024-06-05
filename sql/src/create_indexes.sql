a9065878711ed4646bc3bc544a4e17ec6c5266de
azhang7886 <alexz94264@gmail.com>
2024-06-04 20:19:12 -0700

    UPDATE: moved queries.sql into create_indexes.sql

---
+++ sql/src/create_indexes.sql
@@ -0,0 +1,12 @@
+/*
+Create user querey
+*/
+INSERT INTO Users
+VALUES (login, password, 'customer', NULL, phoneNum, numOverDueGames);
+
+/*
+Check for user info
+*/
+SELECT *
+FROM Users
+WHERE login =  AND password =  AND phoneNum = "";
