6f6c496c097f9e7e720d0a486c201914e03a4894
azhang7886 <alexz94264@gmail.com>
2024-06-10 09:59:33 -0700

    UPDATE: added half of function updateProfile

--- sql/src/queries.sql
+++ sql/src/queries.sql
@@ -1,19 +1,40 @@
 /*
-Create and insert user into the querey
-*/
+ * Create and insert user into the querey
+ */
 INSERT INTO Users
 VALUES (login, password, 'customer', NULL, phoneNum, numOverDueGames);
 
 /*
-Check for user info
-*/
+ * User LogIn using login, password, and phonenumber
+ */
 SELECT *
 FROM Users
 WHERE login =  AND password =  AND phoneNum = "";
 
 /*
-Check the current user's info
-*/
+ * View current customer's profile
+ */
 SELECT *
 FROM Users
 WHERE login =  AND password = ;
+
+/*
+ * Update current customer's password
+ */
+ UPDATE Users
+ SET password =  
+ WHERE login =  ;
+
+/*
+ * Get current user's password
+ */
+SELECT password
+FROM Users
+WHERE login =  ;
+
+ /*
+ * Update current customer's phoneNum
+ */
+ UPDATE Users
+ SET phoneNum =  
+ WHERE login =  ;
